5127d0dab7ad20981b1d000f3eca402b295940fc
Patbox <39821509+Patbox@users.noreply.github.com>
2025-09-27 15:42:07 +0200

    Fix rubblemites and drift jelly jump effects

--- src/main/java/eu/pb4/enderscapepatch/mixin/mod/DriftJellyBlockMixin.java
+++ src/main/java/eu/pb4/enderscapepatch/mixin/mod/DriftJellyBlockMixin.java
@@ -1,5 +1,6 @@
 package eu.pb4.enderscapepatch.mixin.mod;
 
+import eu.pb4.factorytools.api.block.model.generic.BlockStateModelManager;
 import net.bunten.enderscape.block.DriftJellyBlock;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.ShapeContext;
@@ -7,6 +8,7 @@ import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerPosition;
 import net.minecraft.network.packet.s2c.play.PlayerPositionLookS2CPacket;
 import net.minecraft.network.packet.s2c.play.PositionFlag;
+import net.minecraft.particle.BlockStateParticleEffect;
 import net.minecraft.particle.ParticleEffect;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
@@ -18,6 +20,7 @@ import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
@@ -26,7 +29,9 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import java.util.Set;
 
 @Mixin(DriftJellyBlock.class)
-public class DriftJellyBlockMixin {
+public abstract class DriftJellyBlockMixin {
+    @Shadow protected abstract void playBounceEffects(World level, BlockPos pos);
+
     @Inject(method = "onEntityLand", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/Entity;setVelocity(Lnet/minecraft/util/math/Vec3d;)V", shift = At.Shift.AFTER))
     private void fixBounce(BlockView level, Entity entity, CallbackInfo ci) {
         if (entity instanceof ServerPlayerEntity player) {
@@ -35,7 +40,9 @@ public class DriftJellyBlockMixin {
                     Set.of(PositionFlag.DELTA_X, PositionFlag.DELTA_Z, PositionFlag.X, PositionFlag.Y, PositionFlag.Z, PositionFlag.X_ROT, PositionFlag.Y_ROT)
             ));
         }
-
+        if (level instanceof World world) {
+            this.playBounceEffects(world, entity.getSteppingPos());
+        }
     }
 
     @SuppressWarnings("OverwriteAuthorRequired")
@@ -43,6 +50,9 @@ public class DriftJellyBlockMixin {
     protected VoxelShape getCollisionShape(BlockState state, BlockView level, BlockPos pos, ShapeContext context) {
         return VoxelShapes.fullCube();
     }
+
+    @Redirect(method = "onLandedUpon", at = @At(value = "INVOKE", target = "Lnet/bunten/enderscape/block/DriftJellyBlock;playBounceEffects(Lnet/minecraft/world/World;Lnet/minecraft/util/math/BlockPos;)V"))
+    private void dont(DriftJellyBlock instance, World y, BlockPos z) {}
 
     @Redirect(method = "playBounceEffects", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;isClient()Z", ordinal = 1))
     private boolean fakeClient(World instance) {
@@ -52,7 +62,7 @@ public class DriftJellyBlockMixin {
     @Redirect(method = "playBounceEffects", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;addParticleClient(Lnet/minecraft/particle/ParticleEffect;DDDDDD)V"))
     private void fixParticles(World instance, ParticleEffect parameters, double x, double y, double z, double velocityX, double velocityY, double velocityZ) {
         if (instance instanceof ServerWorld serverWorld) {
-            serverWorld.spawnParticles(parameters, x, y, z, 0, velocityX, velocityY, velocityZ, 1);
+            serverWorld.spawnParticles(BlockStateModelManager.getParticle(((BlockStateParticleEffect) parameters).getBlockState()), x, y, z, 0, velocityX, velocityY, velocityZ, 1);
         }
     }
 }
--- src/main/java/eu/pb4/enderscapepatch/mixin/mod/RubblemiteMixin.java
+++ src/main/java/eu/pb4/enderscapepatch/mixin/mod/RubblemiteMixin.java
@@ -1,5 +1,6 @@
 package eu.pb4.enderscapepatch.mixin.mod;
 
+import eu.pb4.enderscapepatch.impl.EnderscapePolymerPatch;
 import eu.pb4.enderscapepatch.impl.entity.BasePolymerEntity;
 import eu.pb4.factorytools.api.virtualentity.emuvanilla.poly.SimpleEntityModel;
 import eu.pb4.enderscapepatch.impl.entity.model.EntityModels;
@@ -10,12 +11,16 @@ import net.bunten.enderscape.entity.rubblemite.Rubblemite;
 import net.bunten.enderscape.entity.rubblemite.RubblemiteVariant;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.data.DataTracked;
 import net.minecraft.entity.data.TrackedData;
+import net.minecraft.entity.data.TrackedDataHandler;
 import net.minecraft.registry.entry.RegistryEntry;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(Rubblemite.class)
 public abstract class RubblemiteMixin extends LivingEntity {
@@ -37,5 +42,10 @@ public abstract class RubblemiteMixin extends LivingEntity {
                 );
             }
         }
+    }
+
+    @Redirect(method = "<clinit>", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/data/DataTracker;registerData(Ljava/lang/Class;Lnet/minecraft/entity/data/TrackedDataHandler;)Lnet/minecraft/entity/data/TrackedData;"))
+    private static TrackedData<Object> forceFakeForSerializerCheckSkip(Class<? extends DataTracked> entityClass, TrackedDataHandler<Object> dataHandler) {
+        return new TrackedData<>(EnderscapePolymerPatch.FAKE_TRACKER_INDEX, dataHandler);
     }
 }
